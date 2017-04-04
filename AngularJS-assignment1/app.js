01bc956c5151da250239be5a28f5efaf08d595db
Lori Li <viola.ljr@gmail.com>
2017-04-04 15:09:07 -0700

    AngularJS-assignment1 bonus completed

--- AngularJS-assignment1/app.js
+++ AngularJS-assignment1/app.js
@@ -12,15 +12,45 @@ function LunchCheckerController ($scope) {
 
   $scope.showMsg = function () {
     var arrayOfDishes = $scope.dishes.split(',');
+    //calculate number of dishes, do NOT consider and empty item, i.e., , , as an item
+    var numberOfDishes = calculate(arrayOfDishes);
 
-    if (arrayOfDishes[0] === "") {
+    //Get the element to modify color
+    var elem1 = document.getElementById("msg");
+    var elem2 = document.getElementById("lunch-menu");
+
+    if (numberOfDishes === 0) {
       $scope.stateOfBeing = "Please enter data first...";
-    } else if (arrayOfDishes.length <= 3){
+      elem1.setAttribute("style", "color: red");
+      elem2.setAttribute("style", "border-color: red");
+    } else if (numberOfDishes <= 3){
       $scope.stateOfBeing = "Enjoy!";
+      elem1.setAttribute("style", "color: green");
+      elem2.setAttribute("style", "border-color: green");
     } else {
       $scope.stateOfBeing = "Too much!";
+      elem1.setAttribute("style", "color: green");
+      elem2.setAttribute("style", "border-color: green");
     }
   };
+  
+  //Count the number of items, do NOT consider and empty item, i.e., , , as an item
+  function calculate(array) {
+    var number = 0;
+    for(var i in array) {
+      if(array[i] === "") continue;
+      //console.log(array[i])
+      for(var j in array[i]) {
+        if(array[i][j] !== " ") {
+          number++;
+          break;
+        }
+      }
+    }
+    return number;
+  }
+
+
 }
 
 })();
